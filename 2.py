46e4ef5ab1fdc3dba130af9eeee37b864e9e021b
jmvianna <jmdvianna@gmail.com>
2026-04-28 16:03:21 -0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("quem é joão vianna?")
+
+st.write("joão vianna é apenas um menino carioca estudante universitário e super legal")
+
+st.image("https://static.wikia.nocookie.net/pure-good-wiki/images/1/18/SpectacularPeterParker.png/revision/latest?cb=20220821054504")
+
+if st.button("voltar"):
+    st.switch_page("1.py")

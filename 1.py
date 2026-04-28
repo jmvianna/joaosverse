3f6aacb5ec0e84125faf6f6dbe1c97a7e6f13b87
jmvianna <jmdvianna@gmail.com>
2026-04-28 15:51:20 -0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title("joaosverse version 1")
 st.image("https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/nationalgeographic_2808031_bx.webp?w=760&h=434")
+
+if st.button("quem é joão vianna?"):
+    st.switch_page("pages/2.py")
+
+if st.button("interesses"):
+    st.switch_page("pages/3.py")

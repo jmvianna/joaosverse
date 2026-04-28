c0b90a0b75c132e3ce077272203cc4647584dff1
jmvianna <jmdvianna@gmail.com>
2026-04-28 16:03:43 -0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,7 +5,7 @@ st.title("joaosverse version 1")
 st.image("https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/nationalgeographic_2808031_bx.webp?w=760&h=434")
 
 if st.button("quem é joão vianna?"):
-    st.switch_page("pages/2.py")
+    st.switch_page("2.py")
 
 if st.button("interesses"):
-    st.switch_page("pages/3.py")
+    st.switch_page("3.py")

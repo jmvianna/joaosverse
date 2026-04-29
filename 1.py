84dbc638a1a7e455bf990f08735fc365c4eeff6f
jmvianna <jmdvianna@gmail.com>
2026-04-29 11:30:17 -0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -7,7 +7,7 @@ st.image("https://static.nationalgeographicbrasil.com/files/styles/image_3200/pu
 opcao = st.selectbox("quem é joão vianna?:", ["definição", "interesses"])
 
 if opcao == "definição":
-     st.write("st.title("definição")
+    st.write("st.title("definição")
     st.write("joão vianna é apenas um menino carioca estudante universitário e super legal")
     st.image("https://static.wikia.nocookie.net/pure-good-wiki/images/1/18/SpectacularPeterParker.png/revision/latest?cb=20220821054504")
 

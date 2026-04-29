5b08833ae2df29816621c202370442af1fc3cbc7
jmvianna <jmdvianna@gmail.com>
2026-04-29 13:27:35 -0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -18,25 +18,28 @@ st.image(
     use_container_width=True
 )
 
-st.divider()
-opcao = st.selectbox("quem é joão vianna?:", ["definição", "interesses"])
+opcao = st.selectbox(
+    "quem é joão vianna?:",
+    ["selecione uma opção", "definição", "interesses"]
+)
 
 if opcao == "definição":
     st.title("definição")
     st.write("joão vianna é apenas um menino carioca estudante universitário e super legal")
     st.image("https://static.wikia.nocookie.net/pure-good-wiki/images/1/18/SpectacularPeterParker.png/revision/latest?cb=20220821054504")
 
-if opcao == "interesses":
-    st.title("interesses")
-
-    st.write("cinema")
-
-    st.image("https://images.adsttc.com/media/images/58d5/3a58/e58e/ce48/a700/003f/large_jpg/002.jpg?1490369108") 
-
-    st.write("marketing e tecnologia")
-
-    st.image("https://estudenapuc.pucpr.br/pos-graduacao/wp-content/uploads/2024/11/MBA-em-Marketing-Digital-Tecnologia-e-IA.jpg")
-
-    st.write("viagens")
-
-    st.image("https://palacepraia.com.br/wp-content/uploads/2021/10/viajar-de-aviao.jpeg")
+elif opcao == "interesses":
+    st.write("joão tem diversos interesses, qual deles você gostaria de aprofundar?")
+    opcao = st.selectbox(
+    "interesses especificados:",
+    ["selecione uma opção", "cinema", "marketing e tecnologia", "viagens"]
+        if opcao == "cinema"
+            st.title("cinema")
+            st.image("https://images.adsttc.com/media/images/58d5/3a58/e58e/ce48/a700/003f/large_jpg/002.jpg?1490369108") 
+            st.write("joão é apaixonado por cinema, alguns de seus filmes favoritos são:")
+            st.write("# star wars: the empire strikes back, 1980")
+            st.image("https://m.media-amazon.com/images/M/MV5BMTkxNGFlNDktZmJkNC00MDdhLTg0MTEtZjZiYWI3MGE5NWIwXkEyXkFqcGc@._V1_.jpg")
+    
+       if opcao == "marketing e tecnologia":
+           st.title("marketing e tecnologia")
+           st.image("https://images.adsttc.com/media/images/58d5/3a58/e58e/ce48/a700/003f/large_jpg/002.jpg?1490369108") 
